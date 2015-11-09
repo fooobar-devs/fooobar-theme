1dd4ecdd582756617647e4f93e10922e092c9c6e
Pierre Dargham <pierre.dargham@gmail.com>
2015-11-09 01:50:42 +0100

    Fix nav-menu and theme scss

--- page-page-1.php
+++ page-page-1.php
@@ -1,3 +1,5 @@
+<div class="container foobar-theme" role="main">
+
       <div class="page-header">
         <h1>Buttons</h1>
       </div>
@@ -516,3 +518,5 @@
       <div class="well">
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sed diam eget risus varius blandit sit amet non magna. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Cras mattis consectetur purus sit amet fermentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Aenean lacinia bibendum nulla sed consectetur.</p>
       </div>
+    
+</div>
